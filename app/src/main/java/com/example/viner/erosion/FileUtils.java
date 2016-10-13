c31bee1b05470cb8ee43985fb9c6697b32be92b6
omerviner <example@example.com>
2016-10-13 17:37:48 +0300

    Cleaned FileUtils

--- app/src/main/java/com/example/viner/erosion/FileUtils.java
+++ app/src/main/java/com/example/viner/erosion/FileUtils.java
@@ -28,7 +28,6 @@ import java.util.concurrent.locks.Lock;
  */
 public class FileUtils {
 
-
     private static final int MAX_SIZE = 512;
 
     /**
@@ -45,7 +44,6 @@ public class FileUtils {
             mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                     Environment.DIRECTORY_PICTURES), "Caimera");
         }
-
 
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
@@ -65,89 +63,9 @@ public class FileUtils {
 
         Log.v("getOutputMediaFile:", path);
         File curImage = new File(path);
-//        try{
-//            curImage.createNewFile();
-//        } catch (Exception e){
-//            Log.v("create new file", "caught exception");
-//        }
 
-        //DialogHelper.showDialog( "Success!","Your picture has been saved!",this);
-//        Toast.makeText(context, "Your picture has been saved!", Toast.LENGTH_SHORT)
-//                .show();
         return curImage;
     }
-
-//    public static byte[] getCapturedData(Context context, byte[] data, int rotation) {
-//
-//        byte[] croppedData = cropAndRotateImageBytes(context, data, rotation);
-//        return croppedData;
-//    }
-//
-//    public static byte[] copyFile(Context context, File file) {
-//        File pictureFile = getOutputMediaFile(context, true);
-//
-//        if (pictureFile == null) {
-//            Toast.makeText(context, "Image retrieval failed.", Toast.LENGTH_SHORT)
-//                    .show();
-//            return null;
-//        }
-//
-//        byte[] data = new byte[(int) file.length()];
-//        try {
-//            new FileInputStream(file).read(data);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//
-//            FileOutputStream fos = new FileOutputStream(pictureFile, true);
-//            fos.write(data);
-//            fos.close();
-//
-//        } catch (FileNotFoundException e) {
-//            Log.v("Error saving: ", e.toString());
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            Log.v("Error saving: ", e.toString());
-//            e.printStackTrace();
-//        }
-//        return data;
-//
-//    }
-//
-//    public static File saveImageToFile(Context context, byte[] data, int rotation, boolean saveStyle) {
-//        File pictureFile = getOutputMediaFile(context, saveStyle);
-//        try {
-//            pictureFile.createNewFile();
-//        } catch (Exception e) {
-//
-//        }
-//
-//        if (pictureFile == null) {
-//            Toast.makeText(context, "Image retrieval failed.", Toast.LENGTH_SHORT)
-//                    .show();
-//            return null;
-//        }
-//        byte[] croppedData = cropAndRotateImageBytes(context, data, rotation);
-//
-//        try {
-//
-//            FileOutputStream fos = new FileOutputStream(pictureFile, true);
-//            fos.write(croppedData);
-//            fos.close();
-//
-//            // Restart the camera preview.
-//        } catch (FileNotFoundException e) {
-//            Log.v("Error saving: ", e.toString());
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            Log.v("Error saving: ", e.toString());
-//            e.printStackTrace();
-//        }
-//        Log.v("saveFileToImage", pictureFile.getAbsolutePath());
-//        return pictureFile;
-//    }
 
     public static Bitmap cropAndRotateImageBytes(Context mContext, byte[] data, int rotation) {
 
@@ -164,17 +82,9 @@ public class FileUtils {
         Bitmap cropped = Bitmap.createBitmap(bitmap, 0, 0, size, size, matrix, true);
         size = Math.min(size, MAX_SIZE);
         cropped = Bitmap.createScaledBitmap(cropped, size,size, false);
-//        cropped.compress(Bitmap.CompressFormat.JPEG, 100, bos); // 100 (best quality)
-//        byte[] square = bos.toByteArray();
         return cropped;
     }
 
-//    public static Bitmap RotateBitmap(Bitmap source, float angle)
-//    {
-//        Matrix matrix = new Matrix();
-//        matrix.postRotate(angle);
-//        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
-//    }
 
     public static Bitmap getCroppedRotatedBitmap(Context context, byte[] data, int rotation) {
 
@@ -209,9 +119,7 @@ public class FileUtils {
         @Override
         protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
             Matrix matrix = new Matrix();
-
             matrix.postRotate(rotateRotationAngle);
-
             return Bitmap.createBitmap(toTransform, 0, 0, toTransform.getWidth(), toTransform.getHeight(), matrix, true);
         }
 
